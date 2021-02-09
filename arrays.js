6e2fdc2cfa66661734787c5a13ad9fcb6d6d3064
Samuel Wilson <samwil56@gmail.com>
2021-02-09 18:19:41 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -8,3 +8,8 @@ function addElementToBeginningOfArray(array,element) {
   var newarray = [element,...array]
   return newarray
 }
+
+function destructivelyAddElementToBeginningOfArray(array,element) {
+  array.unshift(element)
+  return array
+}
